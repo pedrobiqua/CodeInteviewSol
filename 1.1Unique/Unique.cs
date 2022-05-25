fc209992fbaca3348aa7322a48c8eb8696b2bf09
Pedro <pedrobiqua@gmail.com>
2022-05-25 18:43:33 -0300

    Add comentário

--- 1.1Unique/Unique.cs
+++ 1.1Unique/Unique.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace _1._1Unique
 {
-    class Unique
+    public class Unique
     {
+        /// <summary>
+        /// Esse metódo retorna true ou false se houver caracteres repitidos
+        /// </summary>
+        /// <paramref name="str">Palavra a ser análisada.<paramref/>
         public bool IsUniqueChars(string str)
         {
             if (str.Length > 256) return false;
