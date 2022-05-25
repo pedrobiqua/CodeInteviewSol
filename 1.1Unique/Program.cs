4228c0ddbfe7180c1c3b78d5e0009ea675db8cda
Pedro <pedrobiqua@gmail.com>
2022-05-25 18:27:13 -0300

    cOMENTÁRIOS

--- 1.1Unique/Program.cs
+++ 1.1Unique/Program.cs
@@ -8,11 +8,12 @@ namespace _1._1Unique
 {
     class Program
     {
+        /* 
+ 1.1: Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+      What if you cannot use additional data structures ?
+        */
         static void Main(string[] args)
         {
-            //Is Unique: Implement an algorithm to determine if a string has all unique
-            //characters. What if you cannot use additional data structures ?
-
             //Aplication
             var unique = new Unique();
             Console.WriteLine(unique.IsUniqueChars("!!"));
